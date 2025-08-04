5e6eae3c86eb56e34ca6906fd4441871c901c54f
Dima_V <astrallux777@ukr.net>
2025-08-04 23:25:03 +0300

    04.08.2025_R3-01

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Toaster } from 'react-hot-toast';
 import App from './components/App/App';
 import 'modern-normalize/modern-normalize.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-   <React.StrictMode>
+   <QueryClientProvider client={queryClient}>
     <Toaster position="top-center" />
     <App />
-  </React.StrictMode>
+     <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
+
+
+
+
+
+
+
